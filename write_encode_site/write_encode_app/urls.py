96e314927eaf587e575b7511c7c918669a0a4f30
jlotto8 <jlotto8@gmail.com>
2022-12-28 15:14:19 -0500

    views and urls

--- write_encode_site/write_encode_app/urls.py
+++ write_encode_site/write_encode_app/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from . import views
+from write_encode_app.views import TranslatorView, HistoryView
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('', TranslatorView.as_view(), name='translator'),
+    path('<str:translator', HistoryView.as_view(), name='history')
+]
