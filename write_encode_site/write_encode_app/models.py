8e90c46a0f9a1ddfc67f1c9eeb5726861a3740b2
Ahmad J. Bright <ajbright213@gmail.comclear>
2023-01-03 18:58:55 -0500

    created forms.py

--- write_encode_site/write_encode_app/models.py
+++ write_encode_site/write_encode_app/models.py
@@ -5,7 +5,7 @@ class Input(models.Model):
     content = models.CharField(max_length=9999, unique=True)
     character_length = models.CharField(max_length=4)
 
-class Sessions(models.Model):
+class Session(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     session_key = models.CharField(max_length=50)
     inputs = models.ManyToManyField(Input)
