93e08dec3cd7647c7f442b2add98b6b427882aa6
Bryan M. Wolf <bmwolf.professional@gmail.com>
2023-01-10 05:54:21 -0700

    updated urls.py

--- write_encode_site/write_encode_app/urls.py
+++ write_encode_site/write_encode_app/urls.py
@@ -3,7 +3,6 @@ from . import views
 from write_encode_app.views import TranslatorView, HistoryView
 
 urlpatterns = [
-    # path('', views.index, name='index'),
     path('', TranslatorView.as_view(), name='translator'),
     path('history/', HistoryView.as_view())
 ]
